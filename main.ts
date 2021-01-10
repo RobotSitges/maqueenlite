8a9f790baf550a1ad698e877f212a807f5c0a4d4
RobotSitges <44520839+RobotSitges@users.noreply.github.com>
2021-01-10 16:03:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.showIcon(IconNames.Ghost)
 basic.forever(function () {
-    basic.showString("T: " + Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C))
-    basic.showString("H: " + Environment.octopus_BME280(Environment.BME280_state.BME280_humidity))
-    basic.showString("P: " + Environment.octopus_BME280(Environment.BME280_state.BME280_pressure))
-    basic.showString("A: " + Environment.octopus_BME280(Environment.BME280_state.BME280_altitude))
+    basic.showString("T: " + Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C) + "ºC")
+    basic.showString("H: " + Environment.octopus_BME280(Environment.BME280_state.BME280_humidity) + "%")
+    basic.showString("P: " + Environment.octopus_BME280(Environment.BME280_state.BME280_pressure) + "hPa")
+    basic.showString("A: " + Environment.octopus_BME280(Environment.BME280_state.BME280_altitude) + "m")
 })
